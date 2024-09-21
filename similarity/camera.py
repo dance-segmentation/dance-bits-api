d4d37de32be761c39a3b704bff336619f4ab4567
Arpad Dusa <dusarp@gmail.com>
2024-09-21 12:05:58 +0200

    add helper functions

---
+++ similarity/camera.py
@@ -0,0 +1,2 @@
+#this code will utinize the camera to take a video and save it to the local directory
+#this wideo file will be used to compare the similarity between the video and the dance tutorial video 
